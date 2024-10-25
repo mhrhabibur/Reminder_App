17bc1765f941cecb4fc2b50ea79594320a9939e2
Habibur Rahman <mhr.habibur@gmail.com>
2024-10-25 15:15:48 +0600

    first commits

---
+++ Reminder App/Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reminder App
+//
+//  Created by Habibur Rahman on 10/25/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Reminder App"
+    }
+
+
+}
+
